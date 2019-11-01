2a0a055488b3607aea877775b55293457e9f1f9b
Darryl <dswoods@gmail.com>
2019-11-01 13:25:55 -0700

    Make compatible with Moleculer 0.14.x

--- src/action.ts
+++ src/action.ts
@@ -1,59 +1,32 @@
-import { Action, Context } from "moleculer";
+import { ActionSchema } from "moleculer";
 import { isFunction } from "util";
 
 import {
     getMetadata,
-    removeMetadata,
     setMetadata,
 } from "./utils/metadata";
-import { getParamNames } from "./utils/parameters";
 
-export interface ActionOptions {
-    name?: Action["name"];
-    cache?: Action["cache"];
-    metrics?: Action["metrics"];
-    params?: Action["params"];
-}
-
-export function action(options?: ActionOptions): MethodDecorator {
+export function action(options?: ActionSchema): MethodDecorator {
     return <T>(target: Object, propertyKey: string | symbol, descriptor: TypedPropertyDescriptor<T>) => {
         const func: any = descriptor.value;
         if (func && isFunction(func)) {
-            const keyName: string = propertyKey.toString();
-            const opts: ActionOptions = { name: keyName, ...options };
+            const opts: ActionSchema = { ...options };
             const actions = getMetadata(target, "actions") || {};
-            const params = getMetadata(target, `${keyName}Params`);
-            const contextParam = getMetadata(target, `${keyName}Context`);
-            const metaParam = getMetadata(target, `${keyName}Meta`);
-            if (!(opts.params || !params)) {
-                descriptor.value = ((ctx: Context) => {
-                    const args = getParamNames(func).map((param) => {
-                        return ctx.params[param];
-                    });
-
-                    if (contextParam) {
-                        args.splice(contextParam.index, 0, ctx);
-                    }
-
-                    if (metaParam) {
-                        args.splice(metaParam.index, 0, ctx.meta);
-                    }
-
-                    return (func as Function).call(ctx.service, ...args);
-                }) as any;
-            }
+            // const params = getMetadata(target, `${keyName}Params`);
+            // const contextParam = getMetadata(target, `${keyName}Context`);
+            // const metaParam = getMetadata(target, `${keyName}Meta`);
             const handler = descriptor.value;
 
-            if (params) {
-                opts.params = params;
-            }
+            // if (params) {
+            //     opts.params = params;
+            // }
 
             actions[propertyKey] = {
                 handler,
                 ...opts,
             };
             setMetadata(target, "actions", actions);
-            removeMetadata(target, `${keyName}Params`);
+            // removeMetadata(target, `${keyName}Params`);
 
             return descriptor;
         } else {
